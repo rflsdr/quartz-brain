ed12cf4e7752caa277afe79c8a0d379357fda05c
Rafael <sedrakyan.rafael@gmail.com>
2026-05-21 17:50:32 -0400

    change baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "rflsdr.github.io/quartz-brain",
+    baseUrl: "rafael.wiki",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
