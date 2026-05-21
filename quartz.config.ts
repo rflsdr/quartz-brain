d6fc3cd2114cee65e5d89156635a3c255a141707
Rafael <sedrakyan.rafael@gmail.com>
2026-05-21 03:57:37 -0400

    Quartz sync: May 21, 2026, 3:57 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "rflsdr.github.io/quartz-brain",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
